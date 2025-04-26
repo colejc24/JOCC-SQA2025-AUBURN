4ba86f563c1128823999a4acbe80756e11ffa995
Jackson Phillips <60822114+Jacksonmj77@users.noreply.github.com>
2025-04-26 08:32:49 -0500

    added logging to four other functions

--- graphtaint.py
+++ graphtaint.py
@@ -1,14 +1,13 @@
-'''
-Akond Rahman 
-May 04, 2021 
-Construct taint graphs based on weakness types 
-'''
 import constants
 import parser 
 import os 
 from itertools import combinations
+import logging
+
+logger = logging.getLogger(__name__)
 
 def getYAMLFiles(path_to_dir):
+    logger.info(f"Searching for YAML files in directory: {path_to_dir}")
     valid_  = [] 
     for root_, dirs, files_ in os.walk( path_to_dir ):
        for file_ in files_:
@@ -16,6 +15,7 @@ def getYAMLFiles(path_to_dir):
            if(os.path.exists(full_p_file)):
              if (full_p_file.endswith( constants.YAML_EXTENSION  ) or full_p_file.endswith( constants.YML_EXTENSION  )  ):
                valid_.append(full_p_file)
+    logger.info(f"Found {len(valid_)} YAML files.")
     return valid_ 
 
 def constructHelmString(hiera_tuple): 
@@ -25,178 +25,143 @@ def constructHelmString(hiera_tuple):
         str2ret =   constants.DOT_SYMBOL +  constants.HELM_VALUE_KW + constants.DOT_SYMBOL + upper_key + constants.DOT_SYMBOL + key 
     return str2ret 
 
-def getHelmTemplateContent( templ_dir ):
+def getHelmTemplateContent(templ_dir):
+    logger.info(f"Loading Helm template content from directory: {templ_dir}")
     template_content_dict = {}
-    template_yaml_files =  getYAMLFiles( templ_dir )
+    template_yaml_files = getYAMLFiles(templ_dir)
     for template_yaml_file in template_yaml_files:
-        value_as_str      = parser.readYAMLAsStr( template_yaml_file )
+        value_as_str = parser.readYAMLAsStr(template_yaml_file)
         template_content_dict[template_yaml_file] = value_as_str
+    logger.info(f"Loaded {len(template_content_dict)} template files.")
     return template_content_dict 
-
 
 def getMatchingTemplates(path2script, hierarchy_ls):
     templ_list = [] 
     template_content_dict, helm_string_list = {}, []
-    templateDirOfHelmValues = os.path.dirname( path2script )  + constants.TEMPLATES_DIR_KW 
-    if (os.path.exists( templateDirOfHelmValues )  ):
-        template_content_dict = getHelmTemplateContent( templateDirOfHelmValues )
+    templateDirOfHelmValues = os.path.dirname(path2script) + constants.TEMPLATES_DIR_KW 
+    if (os.path.exists(templateDirOfHelmValues)):
+        template_content_dict = getHelmTemplateContent(templateDirOfHelmValues)
     for hiera_ in hierarchy_ls:
-        helm_string_list.append(   constructHelmString( hiera_  ) )
+        helm_string_list.append(constructHelmString(hiera_))
     for template_file, template_string in template_content_dict.items():
         for helm_string in helm_string_list:
-            if helm_string != constants.YAML_SKIPPING_TEXT : 
-                if helm_string in template_string: 
-                    match_count = template_string.count( helm_string  )
+            if helm_string != constants.YAML_SKIPPING_TEXT:
+                if helm_string in template_string:
+                    match_count = template_string.count(helm_string)
                     for _ in range(match_count):
-                        templ_list.append( (template_file, helm_string ) )
+                        templ_list.append((template_file, helm_string))
     return templ_list
 
-def getValidTaints(  lis_template_matches ): 
+def getValidTaints(lis_template_matches): 
     '''
     provides a mapping between the key where the secret occurred and the 
-    files that re affected by teh key 
+    files that are affected by the key 
     '''
-    taint_lis  = []
+    taint_lis = []
     for match in lis_template_matches:
         script_name, helm_string = match 
-        inceptor = helm_string.split( constants.DOT_SYMBOL )[-1]
-        taint_lis.append( (inceptor, script_name) )
+        inceptor = helm_string.split(constants.DOT_SYMBOL)[-1]
+        taint_lis.append((inceptor, script_name))
     return taint_lis 
 
-
-
-def mineSecretGraph( path2script, yaml_dict , secret_dict ):
-    '''
-    This method looks at YAML files in Helm templates. 
-    Works only for secrets. 
-    Need to provide script path, script dict, dictionary of secrets that appear for the script  
-    '''
-
+def mineSecretGraph(path2script, yaml_dict, secret_dict):
+    logger.info(f"Mining secret graph for script: {path2script}")
     within_match_head = None 
     hierarchy_list = []
     for k_, v_ in secret_dict.items():
         for tup_item in v_:
             for value in tup_item:
                 hierarchy_keys = parser.keyMiner(yaml_dict, value)
-                hierarchy_keys = [x_ for x_ in hierarchy_keys if x_ != constants.YAML_SKIPPING_TEXT ] 
-                compo_hiera_keys = [ constants.DOT_SYMBOL.join(str_) for str_ in combinations( hierarchy_keys , 2 )] ## take 2 strings at a time 
-                # print(compo_hiera_keys) 
+                hierarchy_keys = [x_ for x_ in hierarchy_keys if x_ != constants.YAML_SKIPPING_TEXT] 
+                compo_hiera_keys = [constants.DOT_SYMBOL.join(str_) for str_ in combinations(hierarchy_keys, 2)]
                 for h_key in hierarchy_keys:
-                    hierarchy_list.append( (h_key, k_ , v_) )
-                '''
-                the purpose of composite hierarchy keys is to get nested values that are referenced 
-                taking 2 strings at a time 
-                '''
+                    hierarchy_list.append((h_key, k_, v_))
                 for compo_h_key in compo_hiera_keys:
-                    hierarchy_list.append( ( compo_h_key, k_, v_  ) )
+                    hierarchy_list.append((compo_h_key, k_, v_))
     
     templ_match_list = []
-    if( parser.checkIfValidHelm(path2script) ):                    
-        templ_match_list = getMatchingTemplates( path2script, hierarchy_list  )
+    if(parser.checkIfValidHelm(path2script)):                    
+        templ_match_list = getMatchingTemplates(path2script, hierarchy_list)
     else:
-        if( len(hierarchy_list) > 0 ):
-            '''
-            check if valueFrom exists 
-            '''
+        if(len(hierarchy_list) > 0):
             if constants.VALU_FROM_KW not in hierarchy_list: 
-                    within_match_head = hierarchy_list[0]
-    valid_taints = getValidTaints( templ_match_list ) 
-    # print( within_match_head ) 
+                within_match_head = hierarchy_list[0]
+    valid_taints = getValidTaints(templ_match_list)
+    logger.info(f"Found {len(valid_taints)} valid taints.")
     return within_match_head, templ_match_list, valid_taints 
 
-
 def getSHFiles(path_to_dir):
-    valid_  = [] 
-    for root_, _, files_ in os.walk( path_to_dir ):
+    valid_ = [] 
+    for root_, _, files_ in os.walk(path_to_dir):
        for file_ in files_:
            full_p_file = os.path.join(root_, file_)
            if(os.path.exists(full_p_file)):
-             if (full_p_file.endswith( constants.SH_EXTENSION  )  ):
+             if (full_p_file.endswith(constants.SH_EXTENSION)):
                valid_.append(full_p_file)
     return valid_ 
 
-
-def readBashAsStr( path_sh_script ):
+def readBashAsStr(path_sh_script):
     _as_str = constants.YAML_SKIPPING_TEXT
-    with open( path_sh_script , constants.FILE_READ_FLAG) as file_:
+    with open(path_sh_script, constants.FILE_READ_FLAG) as file_:
         _as_str = file_.read()
     return _as_str
 
-def getTaintsFromConfigMaps( script_path ):
+def getTaintsFromConfigMaps(script_path):
     list2Return = [] 
-    config_map_dir  = os.path.dirname( script_path )  + constants.SLASH_SYMBOL    
-    script_name     = script_path.replace( config_map_dir, constants.YAML_SKIPPING_TEXT )
-    sh_files = getSHFiles( config_map_dir )
+    config_map_dir = os.path.dirname(script_path) + constants.SLASH_SYMBOL    
+    script_name = script_path.replace(config_map_dir, constants.YAML_SKIPPING_TEXT)
+    sh_files = getSHFiles(config_map_dir)
     for sh_file in sh_files:
-        sh_content = readBashAsStr( sh_file )
+        sh_content = readBashAsStr(sh_file)
         if script_name in sh_content:
-            sh_match_cnt  = sh_content.count( script_name )
-            for l_ in range( sh_match_cnt ):
-                list2Return.append(  sh_file  )
+            sh_match_cnt = sh_content.count(script_name)
+            for l_ in range(sh_match_cnt):
+                list2Return.append(sh_file)
     return list2Return
-    
 
-
-def mineViolationGraph(path2script, yaml_dict, taint_value, k_ ):
-    '''
-    This method looks at YAML files in Helm templates. 
-    Works for all types. 
-    Need to provide script path, script dict, value identified as smell, key for which value occurs 
-    '''
+def mineViolationGraph(path2script, yaml_dict, taint_value, k_):
     hierarchy_list = [] 
     hierarchy_keys = parser.keyMiner(yaml_dict, taint_value)
-    hierarchy_keys = [x_ for x_ in hierarchy_keys if x_ != constants.YAML_SKIPPING_TEXT ] 
-    compo_hiera_keys = [ constants.DOT_SYMBOL.join(str_) for str_ in combinations( hierarchy_keys , 2 )] ## take 2 strings at a time 
-    # print(compo_hiera_keys) 
+    hierarchy_keys = [x_ for x_ in hierarchy_keys if x_ != constants.YAML_SKIPPING_TEXT] 
+    compo_hiera_keys = [constants.DOT_SYMBOL.join(str_) for str_ in combinations(hierarchy_keys, 2)]
     for h_key in hierarchy_keys:
-        hierarchy_list.append( (h_key, k_ , taint_value) )
-    '''
-    the purpose of composite hierarchy keys is to get nested values that are referenced 
-    taking 2 strings at a time 
-    '''
+        hierarchy_list.append((h_key, k_, taint_value))
     for compo_h_key in compo_hiera_keys:
-        hierarchy_list.append( ( compo_h_key, k_, taint_value  ) )
-    
-    templ_match_list = []
-    templ_match_list = getMatchingTemplates( path2script, hierarchy_list  )    
-
+        hierarchy_list.append((compo_h_key, k_, taint_value))
+    templ_match_list = getMatchingTemplates(path2script, hierarchy_list)
     return templ_match_list
 
-def mineServiceGraph( script_path, dict_yaml, src_val ): 
-    '''
-    This method looks at YAML files that have kind:Service , and checks if used in another YAML with kind:Deployment 
-    Works for all types. 
-    Need to provide script path, script dict, value identified as smell 
-    '''
+def mineServiceGraph(script_path, dict_yaml, src_val):
+    logger.info(f"Mining service graph for source value: {src_val}")
     ret_lis = [] 
-    svc_dir     = os.path.dirname( script_path )  + constants.SLASH_SYMBOL    
-    yaml_files  = getYAMLFiles( svc_dir )    
+    svc_dir = os.path.dirname(script_path) + constants.SLASH_SYMBOL    
+    yaml_files = getYAMLFiles(svc_dir)    
     for yaml_f in yaml_files:
-        if( parser.checkIfValidK8SYaml( yaml_f ) ):
-            dict_as_list   = parser.loadMultiYAML( yaml_f )
-            sink_yaml_dict = parser.getSingleDict4MultiDocs( dict_as_list )                    
-            sink_val_li_   = list(  parser.getValuesRecursively(sink_yaml_dict) )
-            if( src_val in sink_val_li_ ) and ( constants.DEPLOYMENT_KW in sink_val_li_ ): 
-                    sink_keys = parser.keyMiner(sink_yaml_dict, src_val)
-                    if constants.K8S_APP_KW in sink_keys: 
-                        ret_lis.append( (yaml_f, sink_keys  ) )
+        if(parser.checkIfValidK8SYaml(yaml_f)):
+            dict_as_list = parser.loadMultiYAML(yaml_f)
+            sink_yaml_dict = parser.getSingleDict4MultiDocs(dict_as_list)                    
+            sink_val_li_ = list(parser.getValuesRecursively(sink_yaml_dict))
+            if(src_val in sink_val_li_) and (constants.DEPLOYMENT_KW in sink_val_li_):
+                sink_keys = parser.keyMiner(sink_yaml_dict, src_val)
+                if constants.K8S_APP_KW in sink_keys:
+                    ret_lis.append((yaml_f, sink_keys))
+    logger.info(f"Found {len(ret_lis)} service graph connections.")
     return ret_lis 
 
-
-def mineNetPolGraph( script_, dict_y, src_val, src_keys ):
+def mineNetPolGraph(script_, dict_y, src_val, src_keys):
     logger.info(f"Analyzing network policy connections for: {script_}")
-    lis2ret     = [] 
-    net_pol_dir = os.path.dirname( script_ )  + constants.SLASH_SYMBOL    
-    yaml_files  = getYAMLFiles( net_pol_dir )    
+    lis2ret = [] 
+    net_pol_dir = os.path.dirname(script_) + constants.SLASH_SYMBOL    
+    yaml_files = getYAMLFiles(net_pol_dir)    
     for yaml_f in yaml_files:
-        if parser.checkIfValidK8SYaml( yaml_f ):
-            dict_as_list   = parser.loadMultiYAML( yaml_f )
-            sink_yaml_dict = parser.getSingleDict4MultiDocs( dict_as_list )                    
-            sink_val_li_   = list( parser.getValuesRecursively(sink_yaml_dict) )
+        if parser.checkIfValidK8SYaml(yaml_f):
+            dict_as_list = parser.loadMultiYAML(yaml_f)
+            sink_yaml_dict = parser.getSingleDict4MultiDocs(dict_as_list)                    
+            sink_val_li_ = list(parser.getValuesRecursively(sink_yaml_dict))
             if src_val in sink_val_li_ and (constants.DEPLOYMENT_KW in sink_val_li_ or constants.POD_KW in sink_val_li_):  
                 sink_keys = parser.keyMiner(sink_yaml_dict, src_val)                
                 for sink_k in sink_keys:
                     if sink_k in src_keys:
-                        lis2ret.append( ( src_val, sink_k ) ) 
+                        lis2ret.append((src_val, sink_k))
     logger.info(f"Network policy links found: {len(lis2ret)}")
     return lis2ret
